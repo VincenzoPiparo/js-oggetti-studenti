8a5f5c476ca37d81b87a92c13d5e15970a0f9ac7
VincenzoPiparo <v.pipar.97@gmai.com>
2021-11-25 16:17:18 +0100

    ARRAY IN

--- js/script.js
+++ js/script.js
@@ -1,29 +1,68 @@
 // ISTRUZIONI
 
-// Creare un oggetto che descriva uno studente, con le seguenti 
-// proprietà: nome, cognome e età.
-// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
-// Creare un array di oggetti di studenti.
-// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
-// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo
-//  oggetto studente inserendo nell’ordine: nome, cognome e età.
+// [V] Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+// [V] Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+// [V] Creare un array di oggetti di studenti.
+// [] Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
+
+
+// [] Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo
+// [] oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+
 
 // ---------------------------------------------------------------------
+// ESEMPIO DI ARRAY DA FARE DOPO
+// const studentData = {
+//     nome: 'Vincenzo',
+//     cognome: 'Piparo',
+//     eta: 24,
+// }
+// const array = [
+
+// ]
+
 
 // 1 Come prima cosa dobbiamo creare un "OGGETTO" che ci permetta di descrivere lo studente
     // SOTTO SEZIONI PUTNO 1
         // Creazione oggetto:
-        const studentData = {
-            nome: 'Vincenzo', 'Luca', 'Giuseppe'
-            cognome: 'Piparo', 'Rossi', 'Amico'
-            età: '24','35', '19'
+        const studentOne = {
+            name: 'Vincenzo',
+            sureName: 'Piparo',
+            age: 24,
         }
 
-// 2 Propietà da chiedere allo studente suddivise in: NOME, COGNOME, ETA'
+// 2 Attraverso un ciclo FOR-IN stampare tutte le propietà dell'oggetto
     // SOTTO SEZIONI PUTNO 2
+        // Creazione ciclo FOR-IN:
+        for (const key in studentOne) {
+            console.log(studentOne[key])
+        }
 
-// 3 Attraverso un ciclo FOR-IN stampare tutte le propietà dell'oggetto
-    // SOTTO SEZIONI PUTNO 3
+// 3 Creare un array di oggetti di studente
+        // SOTTO SEZIONI PUNTO 3
+        // Creiamo l'array:
+        const studentData = [
+            {
+                name: 'Alessio',
+                sureName: 'Crea',
+                age: 23
+            },
+
+            {
+                name: 'Luca',
+                sureName: 'Rossi',
+                age: 32
+            },
+            
+            {
+                name: 'Paolo',
+                sureName: 'Amico',
+                age: 19
+            }
+        ];
+        console.log(studentData)
 
 // 4 Creare un ciclo dove andremo a stampare tutte le propietà per singolo studente
     // SOTTO SEZIONI PUTNO 4
