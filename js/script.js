5d9eff4f51044cd4dc44785bc8fc5d8b05b4a40b
VincenzoPiparo <v.pipar.97@gmai.com>
2021-11-25 16:26:33 +0100

    END

--- js/script.js
+++ js/script.js
@@ -3,12 +3,8 @@
 // [V] Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 // [V] Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 // [V] Creare un array di oggetti di studenti.
-// [] Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
-
-
-
-// [] Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo
-// [] oggetto studente inserendo nell’ordine: nome, cognome e età.
+// [V] Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+// [V] Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 
 
@@ -55,17 +51,26 @@
                 sureName: 'Rossi',
                 age: 32
             },
-            
+
             {
                 name: 'Paolo',
                 sureName: 'Amico',
                 age: 19
             }
         ];
-        console.log(studentData)
 
 // 4 Creare un ciclo dove andremo a stampare tutte le propietà per singolo studente
     // SOTTO SEZIONI PUTNO 4
+    // Creiamo il ciclo per ogni studente:
+    for (const key in studentData) {
+        console.log(studentData[key])
+    }
 
 // 5 Attraverso 3 PROMPT diamo la possibilità all'utente di aggiungere un nuovo oggetto mantenendo l'oridne di : NOMEl, COGNOME, ETA'   
     // SOTTO SEZIONI PUTNO 5
+    // Creiamo i prompt per gli altri studenti:
+     
+    const inYourName = prompt ('Enter your Name')
+    const inYourSecondName = prompt ('Enter your Second Name')
+    const inYourAge = prompt ('Enter your Age')
+
