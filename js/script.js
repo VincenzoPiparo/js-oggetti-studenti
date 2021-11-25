700230be608bbe129e9698c45d4583e8e5742aba
VincenzoPiparo <v.pipar.97@gmai.com>
2021-11-25 15:47:19 +0100

    CREAZIONE OGGETTO STUDENTE

--- js/script.js
+++ js/script.js
@@ -10,16 +10,23 @@
 
 // ---------------------------------------------------------------------
 
-// 1 Come prima cosa dobbiamo creare un "qualcosa" che ci permetta di descrivere lo studente
-
+// 1 Come prima cosa dobbiamo creare un "OGGETTO" che ci permetta di descrivere lo studente
+    // SOTTO SEZIONI PUTNO 1
+        // Creazione oggetto:
+        const studentData = {
+            nome: 'Vincenzo', 'Luca', 'Giuseppe'
+            cognome: 'Piparo', 'Rossi', 'Amico'
+            età: '24','35', '19'
+        }
 
 // 2 Propietà da chiedere allo studente suddivise in: NOME, COGNOME, ETA'
-
+    // SOTTO SEZIONI PUTNO 2
 
 // 3 Attraverso un ciclo FOR-IN stampare tutte le propietà dell'oggetto
-
+    // SOTTO SEZIONI PUTNO 3
 
 // 4 Creare un ciclo dove andremo a stampare tutte le propietà per singolo studente
+    // SOTTO SEZIONI PUTNO 4
 
-
-// 5 Attraverso 3 PROMPT diamo la possibilità all'utente di aggiungere un nuovo oggetto mantenendo l'oridne di : NOMEl, COGNOME, ETA'
+// 5 Attraverso 3 PROMPT diamo la possibilità all'utente di aggiungere un nuovo oggetto mantenendo l'oridne di : NOMEl, COGNOME, ETA'   
+    // SOTTO SEZIONI PUTNO 5
