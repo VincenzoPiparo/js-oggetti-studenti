109fd0ad5c5c7004409e8fc8b8f8fa5bd2a7b8ba
VincenzoPiparo <v.pipar.97@gmai.com>
2021-11-25 15:38:17 +0100

    IMPOSTATO LO SCHEMA PARTE 1

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// ISTRUZIONI
+
+// Creare un oggetto che descriva uno studente, con le seguenti 
+// proprietà: nome, cognome e età.
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+// Creare un array di oggetti di studenti.
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo
+//  oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+// ---------------------------------------------------------------------
+
+// 1 Come prima cosa dobbiamo creare un "qualcosa" che ci permetta di descrivere lo studente
+
+
+// 2 Propietà da chiedere allo studente suddivise in: NOME, COGNOME, ETA'
+
+
+// 3 Attraverso un ciclo FOR-IN stampare tutte le propietà dell'oggetto
+
+
+// 4 Creare un ciclo dove andremo a stampare tutte le propietà per singolo studente
+
+
+// 5 Attraverso 3 PROMPT diamo la possibilità all'utente di aggiungere un nuovo oggetto mantenendo l'oridne di : NOMEl, COGNOME, ETA'
